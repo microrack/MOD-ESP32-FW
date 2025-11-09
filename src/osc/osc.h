90f3649dda5ae31aad007924adc3c69b31643638
glitchcore <mail@s3f.ru>
2025-11-09 14:18:07 +0400

    midi event callback debug

--- src/osc/osc.h
+++ src/osc/osc.h
@@ -9,7 +9,18 @@ AudioOutput update_audio(void) {
 }
 
 void event_callback(ProcessorEventType event_type, ProcessorEvent event) {
-    // TODO: Implement event callback
+    // print note on event
+    if (event_type == EventNoteOn) {
+        Serial.printf("note on: %d, %d, %d\n", event.note.channel, event.note.note, event.note.velocity);
+    }
+    // print note off event
+    if (event_type == EventNoteOff) {
+        Serial.printf("note off: %d, %d, %d\n", event.note.channel, event.note.note, event.note.velocity);
+    }
+    // print cc event
+    if (event_type == EventCc) {
+        Serial.printf("cc: %d, %d, %d\n", event.cc.channel, event.cc.cc, event.cc.value);
+    }
 }
 
 void osc_init(SignalProcessor* signal_processor) {
